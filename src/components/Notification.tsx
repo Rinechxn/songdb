8469e0e071b9b7d0e5105a2cc2c56b1dfc25fa8d
Nawapon Boonjua <amcpethai22@gmail.com>
2024-04-06 00:47:06 +0700

    off notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,10 +1,10 @@
 export default function NotiPopup() {
     return (
         <>
-            <div className="w-full p-4 bg-[#114629] border border-[#3bff93] text-[#3bff93] rounded-lg">
+            {/* <div className="w-full p-4 bg-[#114629] border border-[#3bff93] text-[#3bff93] rounded-lg">
                 <b className="text-sm">Update Information</b>
                 <p className="text-sm">ระบบจะทำการปิดปรับปรุงระบบ Backend  ในวันที่ 24 มีนาคม 2567</p>
-            </div>
+            </div> */}
         </>
     );
 }
